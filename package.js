b0210162be828408985c5a95728c73b361a15e7d
Mike Bannister <notimpossiblemike@gmail.com>
2012-08-11 16:47:22 -0400

    fixed package

--- package.js
+++ package.js
@@ -2,6 +2,7 @@ Package.describe({
   summary: "underscore.string repackaged for Meteor"
 });
 
-Package.on_use(function (api) {
-  api.add_files('lib/underscore.string/lib/underscore.string.js', 'client');
+Package.on_use(function (api, where) {
+  where = where || ['client', 'server'];
+  api.add_files('lib/underscore.string/lib/underscore.string.js', where);
 });
