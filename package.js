395786164f0bf8d13d4ed8c37aa67d544c78a315
Mike Bannister <notimpossiblemike@gmail.com>
2012-09-02 14:47:24 -0400

    make underscore string load itselft

--- package.js
+++ package.js
@@ -4,5 +4,7 @@ Package.describe({
 
 Package.on_use(function (api, where) {
   where = where || ['client', 'server'];
+
   api.add_files('lib/underscore.string/lib/underscore.string.js', where);
+  api.add_files('common.js', where);
 });
